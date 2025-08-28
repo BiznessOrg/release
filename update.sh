5f4308a7deed5161e6b00e68cf9695791938a076
applesweat <sandy.chicu@gmail.com>
2025-08-28 20:20:05 +0300

    updated update script

--- update.sh
+++ update.sh
@@ -1,12 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 REPO="BiznessOrg/release"
-CURRENT_VERSION=$(cat version 2>/dev/null || echo "")
+CURRENT_VERSION=$(cat version.lock 2>/dev/null || echo "")
 LATEST_VERSION=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | jq -r '.tag_name')
 
-if [ -z "$CURRENT_VERSION" ] || [ "$(printf '%s\n' "$CURRENT_VERSION" "$LATEST_VERSION" | sort -V | head -n1)" = "$CURRENT_VERSION" ] && [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]; then
-   curl -L "https://github.com/$REPO/archive/refs/tags/$LATEST_VERSION.zip" -o "packages.zip"
-   unzip -q "packages.zip"
-   rm "packages.zip"
-   echo "$LATEST_VERSION" > version
+for pkg in curl jq stow unzip; do
+   if ! command -v "$pkg" &> /dev/null; then
+      sudo apk add "$pkg"
+   fi
+done
+
+cd $HOME
+
+if [ -z "$CURRENT_VERSION" ] || [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]; then
+   wget "https://github.com/$REPO/releases/download/$LATEST_VERSION/$LATEST_VERSION.zip" \
+      -o update.log -O "packages.zip" &&
+      rm -rf packages 2>/dev/null &&
+      unzip "packages.zip" &&
+      rm "packages.zip" &&
+      echo "$LATEST_VERSION" > version.lock
 fi
+
+cd packages && stow *
