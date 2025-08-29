d7929467ccfdcf2a611ba094d0cd783913585aa9
applesweat <sandy.chicu@gmail.com>
2025-08-29 22:28:31 +0300

    wowwwwwwwww

--- update.sh
+++ update.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env sh
 
+if [ "$(id -u)" -ne 0 ]; then
+	echo "Must run as root!!!"
+	exit 1
+fi
+
 for pkg in curl jq stow unzip; do
-   if ! command -v "$pkg" &> /dev/null; then
-      doas apk add "$pkg"
-   fi
+	apk add "$pkg"
 done
+
+cd $HOME
 
 REPO="BiznessOrg/release"
 CURRENT_VERSION=$(cat version.lock 2>/dev/null || echo "")
 LATEST_VERSION=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | jq -r '.tag_name')
 
-cd $HOME
-
 if [ -z "$CURRENT_VERSION" ] || [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]; then
-   wget "https://github.com/$REPO/releases/download/$LATEST_VERSION/$LATEST_VERSION.zip" \
-      -o update.log -O "packages.zip" &&
-      rm -rf packages 2>/dev/null &&
-      unzip "packages.zip" &&
-      rm "packages.zip" &&
-      echo "$LATEST_VERSION" > version.lock
+	wget "https://github.com/$REPO/releases/download/$LATEST_VERSION/$LATEST_VERSION.zip" \
+		-o update.log -O "packages.zip" &&
+	rm -rf packages 2>/dev/null &&
+	unzip "packages.zip" &&
+	rm "packages.zip" &&
+	echo "$LATEST_VERSION" > version.lock &&
+	cd packages && apk add *.apk
 fi
-
-cd packages && stow *
